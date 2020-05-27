2cab5f9b24a957ccf3d00c9b51b3be12bdb82185
ChristopherHauschild <chrishaus01@gmail.com>
2020-05-27 18:50:30 -0300

    Updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', start)
 
-var inputRed = null
-var inputGreen = null
-var inputBlue = null
+let count = 0
+let inputRed = null
+let inputGreen = null
+let inputBlue = null
 
 function start() {
   inputRed = document.querySelector('#inputRed')
@@ -14,10 +15,12 @@ function start() {
   inputBlue = document.querySelector('#inputBlue')
   inputBlue.addEventListener('change', colorBlueDiv)
   
+  buttonDark = document.querySelector('.dark')
+  buttonDark.addEventListener('click', enabledDark)
 }
 
 function colorRedDiv(event) {
-  var valueRed = document.querySelector('#valueRed') 
+  let valueRed = document.querySelector('#valueRed') 
   valueRed.classList.add("inputActive")
   valueRed.value = event.target.value
 
@@ -25,7 +28,7 @@ function colorRedDiv(event) {
 }
 
 function colorGreenDiv(event) {
-  var valueGreen = document.querySelector('#valueGreen') 
+  let valueGreen = document.querySelector('#valueGreen') 
   valueGreen.classList.add("inputActive")
   valueGreen.value = event.target.value
 
@@ -34,7 +37,7 @@ function colorGreenDiv(event) {
 
 
 function colorBlueDiv(event) {
-  var valueBlue = document.querySelector('#valueBlue') 
+  let valueBlue = document.querySelector('#valueBlue') 
   valueBlue.classList.add("inputActive")
   valueBlue.value = event.target.value
 
@@ -42,17 +45,36 @@ function colorBlueDiv(event) {
 }
 
 function setColors(R, G, B) {
-  var R = valueRed.value
-  var G = valueGreen.value
-  var B = valueBlue.value
+  let R = valueRed.value
+  let G = valueGreen.value
+  let B = valueBlue.value
 
-  var square = document.querySelector('.square')
+  let square = document.querySelector('.square')
   square.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
 
-  var h1 = document.querySelector('h1')
+  let h1 = document.querySelector('h1')
   h1.style.color = `rgb(${R}, ${G}, ${B})`
 
-  var container = document.querySelector('.container')
+  let container = document.querySelector('.container')
   container.style.boxShadow = `1px 2px 2px 0px rgb(${R}, ${G}, ${B})`
 
+}
+
+function enabledDark(event) {
+  count++
+  
+  body = document.querySelector('body')
+  if (count % 2 !== 0) {
+    body.style.background = 'radial-gradient(circle, rgba(88,85,85,1) 0%, rgba(32,35,42,1) 100%)'
+    buttonDark.textContent = 'Light'
+    buttonDark.style.border = '2px solid yellow'
+    let container = document.querySelector('.container')
+    container.style.boxShadow = '3px 3px 3px 0px #fff'
+  } else {
+    console.log('ei')
+    body.style.background = '#0002'
+    buttonDark.textContent = 'Dark'
+    buttonDark.style.border = '1px solid #0001'
+  }
+  
 }
